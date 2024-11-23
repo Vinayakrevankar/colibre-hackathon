7e6ed02c57371c4479d37fdbc32d8e065db8df65
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-23 00:49:31 -0500

    removed irrelavent code

--- frontendd/src/BuyerDashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LogoutButton from './components/LogoutButton';
-
-const BuyerDashboard = () => {
-  return <h1>Welcome to the Buyer Dashboard - Under Construction  <LogoutButton/></h1>;
-};
-
-export default BuyerDashboard;
