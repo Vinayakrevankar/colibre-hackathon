75c3d3d4a3cfee4c228324109dacea295f098480
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-22 22:49:45 -0500

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Login from './components/Login';
-import Signup from './components/Signup';
-
-const App = () => {
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+  import LandingPage from "./components/LandingPage";
+  import Login from './components/Login';
+  import Signup from './components/Signup';
+  
+  const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
+          <Routes>
+          
+            <Route path="/" element={<LandingPage />} />
+  
+            {/* Protected Routes */}
+            <Route
+              path="/login"
+              element={
+                <Login/>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+               <Signup/>
+              }
+            />
+          </Routes>
         </Router>
     );
-};
-
-export default App;
+  };
+  
+  export default App;
